eacf48e6a299c7911cf1b9e5140c4114812d50a6
Yash Hirulkar <65728956+yashhirulkar701@users.noreply.github.com>
2025-05-22 21:29:31 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,6 +9,5 @@ terraform {
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
-  resource_provider_registrations = "none" # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
   features {}
 }
