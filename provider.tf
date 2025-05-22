4fe9503ce4fe4d5a8e22800df3d695894f6986cb
Yash Hirulkar <65728956+yashhirulkar701@users.noreply.github.com>
2025-05-22 21:31:41 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,13 +1,23 @@
 terraform {
+
+  cloud {
+    organization = "myorg"
+
+    workspaces {
+      name = "terraform-azure-virtual-machine"
+    }
+  }
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.0.0"
+      version = "~>3.10.0"
     }
   }
+
+  required_version = ">= 1.2.3"
 }
 
-# Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
 }
